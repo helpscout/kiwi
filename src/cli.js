74b3c20e59102e7fe0e494de03604a4e4c88c4db
ItsJonQ <itsjonq@gmail.com>
2019-02-07 20:06:09 -0500

    Fix cli version

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const { log } = require("./utils");
 const { syncRepo } = require("./syncRepo");
-const pkg = require("./package.json");
+const pkg = require("../package.json");
 
 log("🥝", "", `Kiwi Started! (v${pkg.version})`);
 
